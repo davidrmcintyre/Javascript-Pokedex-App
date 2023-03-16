dfc19acb1ffe6ffd0420896e251b4df2308af5f8
David McIntyre <davidrobert.mcintyre@gmail.com>
2023-03-16 13:39:09 +0100

    Updated JS
    Added the first 6 pokemon for the pokedex app with their; name, height and type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-let pokemonList = [{},{},{}]
+let pokemonList = [{name:'Bulbasaur', height:'0.7m', type:['Grass, Poison'] },
+                    {name:'Ivysaur', height:'1m', type: ['Grass, Poison']},
+                    {name:'Venusaur', height:'2m', type: ['Grass, Poison']},
+                    {name:'Charmander', height:'0.6m', type:'Fire'},
+                    {name:'Charmeleon', height:'1.1m', type:'Fire'},
+                    {name:'Charizard', height:'1.7m', type:['Fire, Flying']}]

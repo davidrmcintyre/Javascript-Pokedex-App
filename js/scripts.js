1363aa368f651b72ea6e0f5442ca61b5b649fb5c
David McIntyre <davidrobert.mcintyre@gmail.com>
2023-03-20 14:10:22 +0100

    Updated scripts.js
    Added two comments for structure.
    Created first for loop to display in the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
+//this is the first JS file I have made.
+
 let pokemonList = [{name:'Bulbasaur', height:'0.7m', type:['Grass, Poison'] },
                     {name:'Ivysaur', height:'1m', type: ['Grass, Poison']},
                     {name:'Venusaur', height:'2m', type: ['Grass, Poison']},
                     {name:'Charmander', height:'0.6m', type:'Fire'},
                     {name:'Charmeleon', height:'1.1m', type:'Fire'},
                     {name:'Charizard', height:'1.7m', type:['Fire, Flying']}]
+
+//This creates the for loop to display the pokemon in the pages DOM.
+
+for (let i = 0; i < pokemonList.length; i++) {
+document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
+}
